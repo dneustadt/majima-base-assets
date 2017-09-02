5b34fa770c9afc65d9a393c54bfe5dc3e5681fb0
David Neustadt <saberdude@googlemail.com>
2017-09-02 15:40:37 +0200

    Initial commit

---
+++ MajimaBaseAssets.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Copyright (c) 2017
+ *
+ * @package   Majima
+ * @author    David Neustadt <kontakt@davidneustadt.de>
+ * @copyright 2017 David Neustadt
+ * @license   MIT
+ */
+
+namespace Plugins\MajimaBaseAssets;
+
+use Majima\PluginBundle\Components\AssetCollection;
+use Majima\PluginBundle\PluginAbstract;
+
+/**
+ * Class MajimaBaseAssets
+ * @package Plugins\MajimaBaseAssets
+ */
+class MajimaBaseAssets extends PluginAbstract
+{
+    /**
+     * @var int
+     */
+    private $priority = 0;
+
+    /**
+     * @return int
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    /**
+     * @return AssetCollection
+     */
+    public function setCssResources()
+    {
+        $assetCollection = new AssetCollection(join(DIRECTORY_SEPARATOR, [__DIR__, 'Resources', 'css', 'src']));
+        $assetCollection->setFrontendAssets([
+            join(DIRECTORY_SEPARATOR, ['frontend', 'all.scss']),
+        ]);
+        $assetCollection->setBackendAssets([
+            join(DIRECTORY_SEPARATOR, ['backend', 'all.scss']),
+        ]);
+        return $assetCollection;
+    }
+
+    /**
+     * @return AssetCollection
+     */
+    public function setJsResources()
+    {
+        $assetCollection = new AssetCollection(join(DIRECTORY_SEPARATOR, [__DIR__, 'Resources', 'js', 'src']));
+        $assetCollection->setFrontendAssets([
+            join(DIRECTORY_SEPARATOR, ['frontend', 'jquery.js']),
+            join(DIRECTORY_SEPARATOR, ['frontend', 'jquery.pubsub.js']),
+        ]);
+        $assetCollection->setBackendAssets([
+            join(DIRECTORY_SEPARATOR, ['backend', 'jquery.alertable.js']),
+            join(DIRECTORY_SEPARATOR, ['backend', 'jquery.contextMenu.js']),
+        ]);
+        return $assetCollection;
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# MajimaBaseAssets
+
+- **License**: MIT
+- **Github Repository**: <https://github.com/dneustadt/majima-assets>
+
+Basic assets for majima demo plugins
+
+See <https://github.com/dneustadt/majima> for more information.
